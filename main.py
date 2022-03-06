edfa0ba206a9b55f80994fc546bdf30025ae9ae7
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2022-03-06 22:34:26 +0900

    CLIを設定

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+from typing import Union, Optional
+
 from loguru import logger
 
 from nulla.logic.backend import Backend
 from nulla.res import initialize_kv
 from nulla.gui.App import MonitorApp
+from argparse import ArgumentParser
 
 
-def main():
+def cli():
+    parser = ArgumentParser('nulla')
+    parser.add_argument('--source', default=0, type=Union[str, int])
+    parser.add_argument('--model', default=None, type=Optional[str])
+
+    args = parser.parse_args()
+    main(src=args.source, model=args.model)
+
+
+def main(src=0, model=None):
     try:
         initialize_kv(debug=True)
-        backend = Backend(src=0)
+        backend = Backend(src=src)
         backend.start()
         app = MonitorApp(backend)
         app.run()
