2f4efcd94978f63aa5066e6173ff38162fd3fb8d
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2022-03-06 22:35:00 +0900

    コメント省略

--- nulla/logic/cv2_to_kivy.py
+++ nulla/logic/cv2_to_kivy.py
@@ -3,9 +3,8 @@ from kivy.core.image import Texture
 
 
 def cv2_to_kivy(image: np.ndarray) -> Texture:
-    texture = Texture.create(size=(image.shape[1], image.shape[0]), colorfmt='bgr',
-                             bufferfmt='ubyte')  # BGRモードで用意,ubyteはデフォルト引数なので指定なくてもよい
-    texture.blit_buffer(image.tostring(), colorfmt='bgr', bufferfmt='ubyte')  # ★ここもここもBGRで指定しないとRGBになって色の表示がおかしくなる
+    texture = Texture.create(size=(image.shape[1], image.shape[0]), colorfmt='bgr', bufferfmt='ubyte')
+    texture.blit_buffer(image.tostring(), colorfmt='bgr', bufferfmt='ubyte')
     texture.flip_vertical()  # 画像を上下反転する
 
     return texture
