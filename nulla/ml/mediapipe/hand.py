a568f7de3f74ddf34800c7cb2ee49c24eccb6c7d
OhkuboSGMS <sinikake.gokiburi@gmail.com>
2022-06-14 23:20:15 +0900

    add mediapipe hands

---
+++ nulla/ml/mediapipe/hand.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+import numpy as np
+
+from nulla.ml.base import MLBase
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+
+class MPHandTracking(MLBase):
+    def __init__(self):
+        self.hand = mp_hands.Hands(model_complexity=0,
+                                   min_detection_confidence=0.5,
+                                   min_tracking_confidence=0.5)
+
+    def __call__(self,frame, *args, **kwargs):
+        frame.flags.writeable = False
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.hand.process(frame)
+        frame.flags.writeable = True
+        return results
+
+    def draw(self, image: np.ndarray, results: Any, *args, **kwargs):
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+        return image
+    def close(self):
+        self.hand.close()
